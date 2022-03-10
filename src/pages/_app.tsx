a44cd286cfa13bd316594dc2a279d72cc7a1c994
Guilhermo Masid <guilhermomasid@gmail.com>
2022-03-10 16:24:14 -0300

    add chakra ui config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,15 @@
+import { ChakraProvider } from '@chakra-ui/react';
 import { AppProps } from 'next/app';
 import { FunctionComponent } from 'react';
 
+import { theme } from '../styles/theme';
+
 const MyApp: FunctionComponent<AppProps> = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 };
 
 export default MyApp;
