f191d8d861937b9b57b723112ffd16e304d515e0
Guilhermo Masid <guilhermomasid@gmail.com>
2022-03-11 16:39:03 -0300

    create input component and add to login screen

---
+++ src/components/Form/Input.tsx
@@ -0,0 +1,42 @@
+import { forwardRef, FunctionComponent, ReactComponentElement } from 'react';
+
+import {
+  FormControl,
+  FormHelperText,
+  FormLabel,
+  Input as ChakraInput,
+  InputProps as ChakraInputProps,
+} from '@chakra-ui/react';
+
+interface InputProps extends ChakraInputProps {
+  name: string;
+  label?: string;
+  helperText?: string;
+}
+
+export const Input: FunctionComponent<InputProps> = forwardRef<
+  HTMLInputElement,
+  InputProps
+>(({ name, label, isInvalid, helperText, ...rest }, ref) => {
+  return (
+    <FormControl>
+      {!!label && <FormLabel htmlFor={name}>{label}</FormLabel>}
+      <ChakraInput
+        id={name}
+        name={name}
+        focusBorderColor="pink.500"
+        bgColor="gray.900"
+        variant="filled"
+        isInvalid={isInvalid}
+        _hover={{ bgColor: 'grey.900' }}
+        errorBorderColor="red.700"
+        size="lg"
+        ref={ref}
+        {...rest}
+      />
+      {isInvalid && (
+        <FormHelperText color="red.700">{helperText}</FormHelperText>
+      )}
+    </FormControl>
+  );
+});
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,53 +1,92 @@
-import {
-  Button,
-  Flex,
-  FormControl,
-  FormLabel,
-  Input,
-  Stack,
-} from '@chakra-ui/react';
+import { Button, Flex, Stack } from '@chakra-ui/react';
+import { Input } from '../components/Form/Input';
+
+import { Controller, useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { isEmpty } from 'lodash';
+import * as yup from 'yup';
+import { useCallback, useMemo } from 'react';
+
+const schema = yup.object().shape({
+  email: yup
+    .string()
+    .email('Digite um e-mail válido')
+    .required('O e-mail é obrigatório'),
+  password: yup.string().required('Senha obrigatória'),
+});
+
+const defaultValues = {
+  email: '',
+  password: '',
+};
 
 const Home = () => {
+  const { control, formState, handleSubmit, register } = useForm({
+    mode: 'onChange',
+    defaultValues,
+    resolver: yupResolver(schema),
+  });
+
+  const { isValid, dirtyFields, errors } = formState;
+
+  const isSubmitButtonDisabled = useMemo(() => {
+    return isEmpty(dirtyFields) || !isValid;
+  }, [dirtyFields, isValid]);
+
+  const onSubmit = useCallback(async (data) => {
+    console.log(data);
+  }, []);
+
   return (
     <Flex w="100vw" h="100vh" justify="center" align="center">
       <Flex
-        flexDirection="column"
         as="form"
+        name="loginForm"
+        noValidate
+        onSubmit={handleSubmit(onSubmit)}
         w="100%"
         maxW={360}
+        flexDirection="column"
         bg="gray.800"
         p="8"
         rounded="lg"
       >
         <Stack spacing="4">
-          <FormControl>
-            <FormLabel htmlFor="email">E-mail</FormLabel>
-            <Input
-              id="email"
-              name="email"
-              type="email"
-              focusBorderColor="pink.500"
-              bgColor="gray.900"
-              variant="filled"
-              _hover={{ bgColor: 'grey.900' }}
-              size="lg"
-            />
-          </FormControl>
-          <FormControl>
-            <FormLabel htmlFor="password">Senha</FormLabel>
-            <Input
-              id="password"
-              name="password"
-              type="password"
-              focusBorderColor="pink.500"
-              bgColor="gray.900"
-              variant="filled"
-              _hover={{ bgColor: 'grey.900' }}
-              size="lg"
-            />
-          </FormControl>
+          <Controller
+            name="email"
+            control={control}
+            render={({ field }) => (
+              <Input
+                label="E-mail"
+                type="email"
+                isInvalid={!!errors.email}
+                helperText={errors?.email?.message}
+                {...field}
+              />
+            )}
+          />
+          <Controller
+            name="password"
+            control={control}
+            render={({ field }) => (
+              <Input
+                label="Senha"
+                type="password"
+                isInvalid={!!errors.password}
+                helperText={errors?.password?.message}
+                {...field}
+              />
+            )}
+          />
         </Stack>
-        <Button type="submit" colorScheme="pink" mt="6" size="lg">
+        <Button
+          type="submit"
+          colorScheme="pink"
+          mt="6"
+          size="lg"
+          disabled={isSubmitButtonDisabled}
+          // isLoading
+        >
           Enviar
         </Button>
       </Flex>
